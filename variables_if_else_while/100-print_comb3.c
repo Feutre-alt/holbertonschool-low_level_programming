d148962dc39ccb2de75d7077628f1bc344162b47
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-17 21:45:10 +0200

    add script 10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+int main(void)
+{
+	int varNum1 = 0;
+	int varNum2 = 1;
+	int varBase = 9;
+	int varBase2 = 2;
+	int varCtr = varBase;
+	int varDctr = varBase2;
+
+	while (varDctr != 10)
+	{
+			if (varCtr != 0)
+			{
+				putchar('0' + varNum1);
+				putchar('0' + varNum2);
+				varNum2++;
+				varCtr--;
+				if (varDctr != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			else
+			{
+				varBase--;
+				varCtr = varBase;
+				varDctr++;
+				varNum1++;
+				varNum2 = 10 - varBase;
+
+			}
+	}
+	putchar('\n');
+	return (0);
+}
+
