83b23bf9ad53245bb76930fb18fb41dc643034bc
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-17 15:30:19 +0200

    add script 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
+
