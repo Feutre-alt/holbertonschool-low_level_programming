ca4a8bec6c0cd963d8ee98b288fca4898c1abb00
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-17 15:08:43 +0200

    add script 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e' || letter == 'q')
+		{
+			letter++;
+		}
+		else
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
