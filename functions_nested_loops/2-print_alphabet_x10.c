bab4b919a963560e42870bc05b731f2c246a7d5b
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-18 16:36:03 +0200

    update script 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,22 @@
 #include "main.h"
-
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always VOID
+ */
 void print_alphabet_x10(void)
 {
 	char x = 'a';
 	int y = 0;
 
-	
 	while (y < 10)
-	{	
+	{
 	x = 'a';
 		while (x <= 'z')
 		{
 		_putchar(x);
 		x++;
 		}
-	
 	_putchar('\n');
 	y++;
 	}
