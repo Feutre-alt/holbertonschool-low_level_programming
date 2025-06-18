437a7018cfd37636805ccb888972135c7b21d944
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-18 15:48:14 +0200

    add script 1 and 1-main.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x != 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
