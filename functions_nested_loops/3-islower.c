99e726f257173d61ee6eaabf49fb0c0a510425d0
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-19 09:00:24 +0200

    add script 3 and main 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+int _islower(int c)
+{
+	c = 75;
+
+	if (c)
+	{
+		return(1);	
+	}
+	else
+	{
+		return(0);
+	}
+}
+
