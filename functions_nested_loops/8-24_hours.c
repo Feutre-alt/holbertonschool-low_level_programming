ed732aa9eb888015392010a633e811c87c1f28c4
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-20 12:28:21 +0200

    add script 8 and update main.h

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 9; h2++)
+		{
+			if (h1 == 2 && h2 == 4)
+			{
+				break;
+			}
+			for (m1 = 0; m1 <= 6; m1++)
+			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					if (m1 == 6 && m2 == 1)
+					{
+						break;
+					}
+					_putchar('0' + h1);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+					_putchar('\n');
+				}
+
+			}
+		}
+
+	}
+}
