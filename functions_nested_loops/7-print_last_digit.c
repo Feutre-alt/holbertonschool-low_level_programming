56f5857e052154d63a33ec1e5cdb9bf05b8f5610
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-20 09:50:07 +0200

    add script 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *@x: int parameter
+ * Return: (x) (Sucess)
+ */
+int print_last_digit(int x)
+{
+	int r = 156688445;
+
+	x = r % 10;
+	_putchar(x);
+	return (x);
+}
+
