ba3e498a6017b7247d0c07a2fed5aea99fe84446
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-20 09:06:50 +0200

    add script 6 and update main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * abs - Entry point
+ *
+ * Return: Return absolute value (Sucess)
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+	else
+	{
+		return (x);
+	}
+}
