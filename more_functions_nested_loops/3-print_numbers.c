745fc227ffa127da6581ba8602642518017fccd7
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-23 10:38:35 +0200

    add script that print numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+void print_numbers(void)
+{
+	char num = 48;
+	while (num < 58)
+	{
+		_putchar(num);
+		num++;
+	}		
+_putchar('\n');
+}
