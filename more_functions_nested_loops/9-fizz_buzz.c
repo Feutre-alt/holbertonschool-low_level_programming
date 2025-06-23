065f97fc2bd977f086ddde8d03ad72d0bc05a2e7
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-23 15:34:49 +0200

    add escape after fizz and buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,13 @@ int main(void)
 	{
 		if (num % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
-		else if (num % 5 == 0)
+		if (num % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
-		else
+		if (num % 5 >= 0 || num % 3 >= 0)
 		{
 			printf("%d", num);
 		}
