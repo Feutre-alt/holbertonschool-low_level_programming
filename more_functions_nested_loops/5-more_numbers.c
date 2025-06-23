fa10d4587963080b7177c6ec9edc48e8dfb0ff02
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-23 13:48:36 +0200

    add script 5 that display numbers from 0 to 14 10 time

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+void more_numbers(void)
+{
+	char num1 = '0';
+	char num2 = '0';
+	int c, b;
+
+	for (c = 0; c <= 9; c++)
+	{
+		b = 0;
+		num2 = '0';
+		num1 = '0';
+		while (b <= 14)
+		{
+			if (num1 == '1')
+			{
+				_putchar(num1);
+			}
+			if (num2 > '9')
+			{
+				num1 = '1';
+				num2 = '0';
+			}
+			_putchar(num2);
+			num2++;
+			b++;
+		}
+		_putchar('\n');
+	}
+}
