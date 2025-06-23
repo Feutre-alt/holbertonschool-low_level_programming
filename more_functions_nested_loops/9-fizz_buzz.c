87b97e8b22ce6a68abddf085a505c5edfa15370a
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-23 15:25:43 +0200

    add script that display number from 0 to 100 and replace multiplier of 3 by fizz and 5 by buss

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) (Sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
