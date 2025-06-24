b6bdce5a922d660c395626fd7657edbfc31a154a
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-24 15:29:01 +0200

    edit script 2 for betty

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always (0) (Sucess)
+ * _strlen - Entry point
+ *@s: name of array
+ * Return: always (compte) (Sucess)
  */
 int _strlen(char *s)
 {
 	int compte = 0;
-	
+
 	while (s[compte] != 0)
 	{
 		compte++;
