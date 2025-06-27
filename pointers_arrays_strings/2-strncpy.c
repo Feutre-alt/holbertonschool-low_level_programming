927f927b6178d5ad699985a3a538505b30b83996
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-27 10:12:13 +0200

    add function that copy a string and update main.h with his prototype

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - Entry point
+ *@dest: destination string
+ *@src: source string
+ *@n: number of character to copy
+ * Return: Always (dest) (Sucess)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int tailleDest = 0;
+	int tailleSrc = 0;
+
+
+	while(tailleSrc < n)
+	{
+		dest[tailleDest] = src[tailleSrc];
+		tailleDest++;
+		tailleSrc++;
+	}
+	dest[tailleDest] = '\0';
+	return (dest);
+}
