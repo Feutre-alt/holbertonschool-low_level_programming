8a6f04198c05160fb06351512d34ccf87cb676f0
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-27 14:53:58 +0200

    add condition for break only when result !=0

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,6 +8,22 @@
 int _strcmp(char *s1, char *s2)
 {
 	int result = s1[0] - s2[0];
+	int i = 0;
+	int i2 = 0;
+
+	while (s1[i] != '\0' || s2[i2] != '\0')
+	{
+		result = s1[i] - s2[i2];
+		if (result != 0)
+		{
+			break;
+		}
+		else
+		{
+			i++;
+			i2++;
+		}
+	}
 
 	return (result);
 }
