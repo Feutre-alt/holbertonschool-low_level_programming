5d035b5916766186fc82533196b268ed20b0a209
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-27 14:32:25 +0200

    add function that compare first char of two string

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcmp - Entry point
+ *@s1: first array
+ *@s2: second array
+ * Return: Always (0) (Sucess)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result = s1[0] - s2[0];
+
+	return (result);
+}
