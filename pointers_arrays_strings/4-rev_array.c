2ec7796bf6092614d2faaeca6a14577015ed42ad
Feutre-alt <loris.pelletier@yahoo.com>
2025-06-27 15:11:59 +0200

    add function that revers a int array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * reverse_array - Entry point
+ *@a: array of int
+ *@n: number of element in the array
+ * Return: Always (0) (Sucess)
+ */
+void reverse_array(int *a, int n)
+{
+	int end = 0;
+	int start = 0;
+	int tmp = 0;
+
+	while (end != n - 1)
+	{
+		end++;
+	}
+	while (end != start)
+	{
+		tmp = a[end];
+		a[end] = a[start];
+		a[start] = tmp;
+		end--;
+		start++;
+	}
+}
