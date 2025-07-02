f1f844c5a47a39df2a63d20d80e272e2084c126a
Feutre-alt <loris.pelletier@yahoo.com>
2025-07-02 14:24:41 +0200

    update style for betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
- *
+ * _strlen_recursion - Entry point
+ *@s: array of char
  * Return: Always (0) (Sucess)
  */
 int _strlen_recursion(char *s)
 {
 	int len = 1;
-	
+
 	if (*s != '\0')
 	{
-		len = len + _strlen_recursion(s + 1); 
+		len = len + _strlen_recursion(s + 1);
 	}
 	else
 	{
