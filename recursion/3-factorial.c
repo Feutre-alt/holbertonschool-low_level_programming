d48f68c69bb9d95abe0188b59183055f27d9dc29
Feutre-alt <loris.pelletier@yahoo.com>
2025-07-02 14:43:01 +0200

    add fonction that print the factorial of an int

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - Entry point
+ *@n: int 
+ * Return: Always (0) (Sucess)
+ */
+int factorial(int n)
+{
+	int fact = n;
+	
+	if (n > 1)
+	{
+		fact = fact * factorial(n - 1);
+	}
+	return (fact);
+	
+}
